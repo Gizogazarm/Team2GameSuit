bb066561c73714abbdfff4de64616a7897eea3f1
Gizogazarm <theblues11.helmi@gmail.com>
2023-03-22 22:49:42 +0700

    awal logic

--- app/src/main/java/com/example/gamesuit/MainActivity.kt
+++ app/src/main/java/com/example/gamesuit/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.gamesuit
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gamesuit.databinding.ActivityMainBinding
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
