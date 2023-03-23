14294c90c36eecdbba021395e80bc34815a7c2d4
Gizogazarm <theblues11.helmi@gmail.com>
2023-03-23 21:45:55 +0700

    menambahkan logic pilihan hanya bisa satu dengan isEnabled

--- app/src/main/java/com/example/gamesuit/MainActivity.kt
+++ app/src/main/java/com/example/gamesuit/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.gamesuit
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.gamesuit.databinding.ActivityMainBinding
 
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pilihanPlayer: String
+    private val pilihanSuit = arrayOf("batu","kertas","gunting")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +24,30 @@ class MainActivity : AppCompatActivity() {
 
             batuPlayer.setOnClickListener {
                 batuPlayer.setBackgroundResource(R.drawable.bg_click)
+                batuPlayer.isEnabled = false
+                kertasPlayer.isEnabled = false
+                guntingPlayer.isEnabled = false
+                pilihanPlayer = pilihanSuit[0]
+                Log.d("pilihan Suit", pilihanPlayer)
+
             }
 
             kertasPlayer.setOnClickListener {
                 kertasPlayer.setBackgroundResource(R.drawable.bg_click)
+                batuPlayer.isEnabled = false
+                kertasPlayer.isEnabled = false
+                guntingPlayer.isEnabled = false
+                pilihanPlayer = pilihanSuit[1]
+                Log.d("pilihan Suit", pilihanPlayer)
             }
 
             guntingPlayer.setOnClickListener {
                 guntingPlayer.setBackgroundResource(R.drawable.bg_click)
+                batuPlayer.isEnabled = false
+                kertasPlayer.isEnabled = false
+                guntingPlayer.isEnabled = false
+                pilihanPlayer = pilihanSuit[2]
+                Log.d("pilihan Suit", pilihanPlayer)
             }
 
         }
