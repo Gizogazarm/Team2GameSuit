d394b57b0a91e307f649a9c87f30d790e5300936
Gizogazarm <theblues11.helmi@gmail.com>
2023-03-26 22:09:18 +0700

    menambahkan fitur dari btnRefresh untuk mereset data agar game dapat dimainkan kembali

--- app/src/main/java/com/example/gamesuit/MainActivity.kt
+++ app/src/main/java/com/example/gamesuit/MainActivity.kt
@@ -12,7 +12,6 @@ import android.widget.TextView
 import com.example.gamesuit.databinding.ActivityMainBinding
 
 
-
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivityMainBinding
 
@@ -20,9 +19,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var pilihanPlayer: String
     private lateinit var hasilSuit: String
-    private val pilihanSuit = arrayOf("batu","kertas","gunting")
+    private lateinit var originalText: String
+    private var originalSize: Float = 0f
+    private var originalColor: Int = 0
+    private val pilihanSuit = arrayOf("batu", "kertas", "gunting")
     private val suitHasil = arrayOf("Pemain Menang", "Computer Menang", "Draw")
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,72 +37,83 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
 
+            originalText = textCenter.text.toString()
+            originalSize = textCenter.textSize
+            originalColor = textCenter.currentTextColor
+
             batuPlayer.setOnClickListener {
                 batuPlayer.setBackgroundResource(R.drawable.bg_click)
-                setEnabledImageView(batuPlayer,kertasPlayer,guntingPlayer,false)
+                setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, false)
                 pilihanPlayer = pilihanSuit[0]
                 player.setPilihanPlayer(pilihanPlayer)
                 Log.d("coba hasil pilihan player", player.getPilihanPlayer())
                 computer.pilihanCom(pilihanSuit)
                 computer.setPilihanPlayer(computer.getPilihanCom())
-                computer.efekPilihanCom(batuCom,guntingCom,kertasCom)
+                computer.efekPilihanCom(batuCom, guntingCom, kertasCom)
                 Log.d("coba hasil komputer", computer.getPilihanPlayer())
-                player.suit(player.getPilihanPlayer(),computer.getPilihanPlayer())
-                computer.suit(computer.getPilihanPlayer(),player.getPilihanPlayer())
+                player.suit(player.getPilihanPlayer(), computer.getPilihanPlayer())
+                computer.suit(computer.getPilihanPlayer(), player.getPilihanPlayer())
                 Log.d("coba hasil status menang player", "${player.getStatusMenang()}")
                 Log.d("coba hasil status menang computer ", "${computer.getStatusMenang()}")
-                hasilSuit = hasilSuit(player.getStatusMenang(),computer.getStatusMenang(),suitHasil)
+                hasilSuit =
+                    hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
                 Log.d("coba hasil status Hasil Suit ", hasilSuit)
-                efekHasilSuit(textCenter,hasilSuit,suitHasil)
-
+                efekHasilSuit(textCenter, hasilSuit, suitHasil)
 
 
             }
 
             kertasPlayer.setOnClickListener {
                 kertasPlayer.setBackgroundResource(R.drawable.bg_click)
-                setEnabledImageView(batuPlayer,kertasPlayer,guntingPlayer,false)
+                setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, false)
                 pilihanPlayer = pilihanSuit[1]
                 player.setPilihanPlayer(pilihanPlayer)
                 Log.d("coba hasil pilihan player", player.getPilihanPlayer())
                 computer.pilihanCom(pilihanSuit)
                 computer.setPilihanPlayer(computer.getPilihanCom())
-                computer.efekPilihanCom(batuCom,guntingCom,kertasCom)
+                computer.efekPilihanCom(batuCom, guntingCom, kertasCom)
                 Log.d("coba hasil komputer", computer.getPilihanPlayer())
-                player.suit(player.getPilihanPlayer(),computer.getPilihanPlayer())
-                computer.suit(computer.getPilihanPlayer(),player.getPilihanPlayer())
+                player.suit(player.getPilihanPlayer(), computer.getPilihanPlayer())
+                computer.suit(computer.getPilihanPlayer(), player.getPilihanPlayer())
                 Log.d("coba hasil status menang player", "${player.getStatusMenang()}")
                 Log.d("coba hasil status menang computer", "${computer.getStatusMenang()}")
-                hasilSuit = hasilSuit(player.getStatusMenang(),computer.getStatusMenang(),suitHasil)
+                hasilSuit =
+                    hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
                 Log.d("coba hasil status Hasil Suit ", hasilSuit)
-                efekHasilSuit(textCenter,hasilSuit,suitHasil)
+                efekHasilSuit(textCenter, hasilSuit, suitHasil)
 
 
             }
 
             guntingPlayer.setOnClickListener {
                 guntingPlayer.setBackgroundResource(R.drawable.bg_click)
-                setEnabledImageView(batuPlayer,kertasPlayer,guntingPlayer,false)
+                setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, false)
                 pilihanPlayer = pilihanSuit[2]
                 player.setPilihanPlayer(pilihanPlayer)
                 Log.d("coba hasil pilihan player", player.getPilihanPlayer())
                 computer.pilihanCom(pilihanSuit)
                 computer.setPilihanPlayer(computer.getPilihanCom())
-                computer.efekPilihanCom(batuCom,guntingCom,kertasCom)
+                computer.efekPilihanCom(batuCom, guntingCom, kertasCom)
                 Log.d("coba hasil komputer", computer.getPilihanPlayer())
-                player.suit(player.getPilihanPlayer(),computer.getPilihanPlayer())
-                computer.suit(computer.getPilihanPlayer(),player.getPilihanPlayer())
+                player.suit(player.getPilihanPlayer(), computer.getPilihanPlayer())
+                computer.suit(computer.getPilihanPlayer(), player.getPilihanPlayer())
                 Log.d("coba hasil status menang player", "${player.getStatusMenang()}")
                 Log.d("coba hasil status menang computer", "${computer.getStatusMenang()}")
-                hasilSuit = hasilSuit(player.getStatusMenang(),computer.getStatusMenang(),suitHasil)
+                hasilSuit = hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
                 Log.d("coba hasil status Hasil Suit ", hasilSuit)
-                efekHasilSuit(textCenter,hasilSuit,suitHasil)
+                efekHasilSuit(textCenter, hasilSuit, suitHasil)
 
             }
 
             btnRefresh.setOnClickListener {
-                setEnabledImageView(batuPlayer,kertasPlayer,guntingPlayer,true)
-                setClearImageView(batuCom,guntingCom,kertasCom)
+                setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, true)
+                setClearImageView(batuCom, guntingCom, kertasCom)
+                textCenter.text = originalText
+                textCenter.setTextColor(originalColor)
+                textCenter.setTextSize(TypedValue.COMPLEX_UNIT_SP,64f)
+                textCenter.setBackgroundResource(R.drawable.bg_awal_click)
+                Log.d("coba hasil original size", "$originalSize")
+
             }
 
 
@@ -109,52 +121,51 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun setClearImageView(imageView1: ImageView,imageView2: ImageView,imageView3: ImageView) {
+    private fun setClearImageView(imageView1: ImageView, imageView2: ImageView, imageView3: ImageView) {
         imageView1.setBackgroundResource(R.drawable.bg_awal_click)
         imageView2.setBackgroundResource(R.drawable.bg_awal_click)
         imageView3.setBackgroundResource(R.drawable.bg_awal_click)
     }
 
 
-    private fun efekHasilSuit(textView: TextView, hasilSuit:String, arrayhasilSuit: Array<String>) {
+    private fun efekHasilSuit(textView: TextView, hasilSuit: String, arrayhasilSuit: Array<String>) {
+
+        when (hasilSuit) {
+            arrayhasilSuit[0] ->
+                textView.apply {
+                    setBackgroundResource(R.drawable.bg_text_center)
+                    text = hasilSuit
+                    setTextColor(Color.WHITE)
+                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+                    Gravity.CENTER
+                }
 
 
-            when(hasilSuit) {
-                arrayhasilSuit[0] ->
-                    textView.apply {
-                        setBackgroundResource(R.drawable.bg_text_center)
-                        text = hasilSuit
-                        setTextColor(Color.WHITE)
-                        setTextSize(TypedValue.COMPLEX_UNIT_SP,18f)
-                        Gravity.CENTER
-                    }
+            arrayhasilSuit[1] ->
+                textView.apply {
+                    setBackgroundResource(R.drawable.bg_text_center)
+                    text = hasilSuit
+                    setTextColor(Color.WHITE)
+                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
+                    Gravity.CENTER
+                }
 
 
-                arrayhasilSuit[1] ->
-                    textView.apply {
-                        setBackgroundResource(R.drawable.bg_text_center)
-                        text = hasilSuit
-                        setTextColor(Color.WHITE)
-                        setTextSize(TypedValue.COMPLEX_UNIT_SP,18f)
-                        Gravity.CENTER
-                    }
-
-
-                arrayhasilSuit[2] ->
-                    textView.apply {
-                        setBackgroundResource(R.drawable.bg_text_center_draw)
-                        text = hasilSuit
-                        setTextColor(Color.WHITE)
-                        setTextSize(TypedValue.COMPLEX_UNIT_SP,24f)
-                        Gravity.CENTER
-                    }
-            }
+            arrayhasilSuit[2] ->
+                textView.apply {
+                    setBackgroundResource(R.drawable.bg_text_center_draw)
+                    text = hasilSuit
+                    setTextColor(Color.WHITE)
+                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+                    Gravity.CENTER
+                }
+        }
     }
 
-    private fun hasilSuit(status1: Boolean,status2: Boolean,arrayhasilSuit: Array<String>): String {
-      lateinit var nilaiString: String
+    private fun hasilSuit(status1: Boolean, status2: Boolean, arrayhasilSuit: Array<String>): String {
+        lateinit var nilaiString: String
 
-        if(status1 && !status2) {
+        if (status1 && !status2) {
             nilaiString = arrayhasilSuit[0]
         } else if (!status1 && status2) {
             nilaiString = arrayhasilSuit[1]
@@ -164,10 +175,10 @@ class MainActivity : AppCompatActivity() {
         return nilaiString
     }
 
-    private fun setEnabledImageView(imageView1: ImageView,imageView2: ImageView,imageView3: ImageView,setNilai: Boolean) {
+    private fun setEnabledImageView(imageView1: ImageView, imageView2: ImageView, imageView3: ImageView, setNilai: Boolean) {
 
 
-        if(setNilai) {
+        if (setNilai) {
             imageView1.isEnabled = setNilai
             imageView1.setBackgroundResource(R.drawable.bg_awal_click)
             imageView2.isEnabled = setNilai
