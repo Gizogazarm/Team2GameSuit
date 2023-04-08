5fb861f0f3402482184dfe17e96e4701336dc0f0
Gizogazarm <theblues11.helmi@gmail.com>
2023-04-08 19:31:10 +0700

    menambahkan splash screen activity

---
+++ app/src/main/java/com/example/gamesuit/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.gamesuit
+
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gamesuit.databinding.ActivitySplashScreenBinding
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
