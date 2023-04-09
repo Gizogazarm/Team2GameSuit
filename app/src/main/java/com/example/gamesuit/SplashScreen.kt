fe42b639d188a872eee4e35369eabe4426761868
Gizogazarm <theblues11.helmi@gmail.com>
2023-04-09 12:26:58 +0700

    menambahkan logic splash screen agar bisa pindah halaman dengan intent

--- app/src/main/java/com/example/gamesuit/SplashScreen.kt
+++ app/src/main/java/com/example/gamesuit/SplashScreen.kt
@@ -2,8 +2,11 @@ package com.example.gamesuit
 
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.bumptech.glide.Glide
 import com.example.gamesuit.databinding.ActivitySplashScreenBinding
 
@@ -19,5 +22,10 @@ class SplashScreen : AppCompatActivity() {
         setContentView(binding.root)
 
         Glide.with(this).load("https://i.ibb.co/HC5ZPgD/splash-screen1.png").into(binding.imageGlide)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000 )
     }
 }
