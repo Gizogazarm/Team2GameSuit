e53bb264023ef081b134bbadc669ec37a6a4905b
Gizogazarm <theblues11.helmi@gmail.com>
2023-04-15 20:30:16 +0700

    menambahkan fun dotSelected agar dapat terpilih dan dot berubah (masih beta belum dicoba)

--- app/src/main/java/com/example/gamesuit/LandingPageActivity.kt
+++ app/src/main/java/com/example/gamesuit/LandingPageActivity.kt
@@ -3,6 +3,7 @@ package com.example.gamesuit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.gamesuit.databinding.ActivityLandingPageBinding
 
 class LandingPageActivity : AppCompatActivity() {
@@ -26,7 +27,7 @@ class LandingPageActivity : AppCompatActivity() {
 
             btnLandingpage.setOnClickListener {
                 val currentItem = viewPager.currentItem
-                if(currentItem == fragments.size-1) {
+                if (currentItem == fragments.size - 1) {
                     val intent = Intent(this@LandingPageActivity, MainActivity::class.java)
                     startActivity(intent)
                     finish()
@@ -34,9 +35,32 @@ class LandingPageActivity : AppCompatActivity() {
                     viewPager.currentItem = viewPager.currentItem + 1
                 }
             }
+            dotSelected(dot1,dot2,dot3)
 
+        }
+    }
 
+    private fun dotSelected(
+        imageDot1: ImageView,
+        imageDot2: ImageView,
+        imageDot3: ImageView
+    ) {
+
+        with(binding) {
+            when (viewPager.currentItem) {
+                0 -> {
+                    imageDot1.setBackgroundResource(R.drawable.ic_landingpage_selected)
+                }
+                1 -> {
+                    imageDot2.setBackgroundResource(R.drawable.ic_landingpage_selected)
+                }
+                else -> {
+                    imageDot3.setBackgroundResource(R.drawable.ic_landingpage_selected)
+                }
+            }
         }
 
     }
+
+
 }
