84734857e876dfdbb58b65e7b4de91d5c1672a02
Gizogazarm <theblues11.helmi@gmail.com>
2023-04-17 23:54:32 +0700

    menambahkan perubahan pada textview akibat perubahan dari intent.putExtra

--- app/src/main/java/com/example/gamesuit/HomeActivity.kt
+++ app/src/main/java/com/example/gamesuit/HomeActivity.kt
@@ -2,10 +2,20 @@ package com.example.gamesuit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gamesuit.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private var namaPemain: String? = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        namaPemain = intent.getStringExtra("simpanNama")
+        binding.textPemainvspemain.text = "$namaPemain vs Pemain"
+        binding.textPemainvscomputer.text = "$namaPemain vs Computer"
+
     }
 }
