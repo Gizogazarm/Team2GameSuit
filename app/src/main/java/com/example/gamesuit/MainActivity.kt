3feb63cd5a16fef5d606e3b13b380d0ff1f7208a
Gizogazarm <theblues11.helmi@gmail.com>
2023-04-19 10:54:54 +0700

    mengubah aturan pemain dengan pemain pertama yang menekan tombol dahulu baru menginisiasi player kedua walupun hasil suit masih bug

--- app/src/main/java/com/example/gamesuit/MainActivity.kt
+++ app/src/main/java/com/example/gamesuit/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.gamesuit
 
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.gamesuit.databinding.ActivityMainBinding
-
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private var simpanNama: String? = ""
     private val pilihanSuit = arrayOf("batu", "kertas", "gunting")
     private val suitHasil = arrayOf("Pemain Menang", "Computer Menang", "Draw")
+    private var firstPlayer = true
     private val player = Player()
     private val computer = Computer()
     private val player2 = Player()
@@ -35,11 +33,12 @@ class MainActivity : AppCompatActivity() {
         with(binding) {
 
             simpanNama = intent.getStringExtra("simpanNama")
-            Glide.with(this@MainActivity).load("https://i.ibb.co/HC5ZPgD/splash-screen1.png").into(imageGlideMain)
+            Glide.with(this@MainActivity).load("https://i.ibb.co/HC5ZPgD/splash-screen1.png")
+                .into(imageGlideMain)
             tagpemain.text = simpanNama
 
             val gameMode = intent.getBooleanExtra("gameMode", false)
-            if (gameMode == false) {
+            if (!gameMode) {
                 tagpemain2.text = "Computer"
                 batuPlayer.setOnClickListener {
                     batuPlayer.setBackgroundResource(R.drawable.bg_click)
@@ -47,8 +46,10 @@ class MainActivity : AppCompatActivity() {
                     pilihanPlayer = pilihanSuit[0]
                     player.setPilihanPlayer(pilihanPlayer)
                     pilihanComputer(batuCom, guntingCom, kertasCom)
-                    duelSuit()
-                    hasilSuit = hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
+                    duelSuit(false)
+                    hasilSuit =
+                        hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
+                    Log.d("pemenang", hasilSuit)
 
                 }
 
@@ -58,9 +59,10 @@ class MainActivity : AppCompatActivity() {
                     pilihanPlayer = pilihanSuit[1]
                     player.setPilihanPlayer(pilihanPlayer)
                     pilihanComputer(batuCom, guntingCom, kertasCom)
-                    duelSuit()
-                    hasilSuit = hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
-
+                    duelSuit(false)
+                    hasilSuit =
+                        hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
+                    Log.d("pemenang", hasilSuit)
                 }
 
                 guntingPlayer.setOnClickListener {
@@ -69,20 +71,188 @@ class MainActivity : AppCompatActivity() {
                     pilihanPlayer = pilihanSuit[2]
                     player.setPilihanPlayer(pilihanPlayer)
                     pilihanComputer(batuCom, guntingCom, kertasCom)
-                    duelSuit()
-                    hasilSuit = hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
+                    duelSuit(false)
+                    hasilSuit =
+                        hasilSuit(player.getStatusMenang(), computer.getStatusMenang(), suitHasil)
+                    Log.d("pemenang", hasilSuit)
 
                 }
 
             } else {
                 tagpemain2.text = "Pemain"
+                setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                batuPlayer.setOnClickListener {
+                    setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, false)
+                    pilihanPlayer = pilihanSuit[0]
+                    player.setPilihanPlayer(pilihanPlayer)
+                    firstPlayer = false
+
+                    if (!firstPlayer) {
+                        setEnabledImageView(batuCom, kertasCom, guntingCom, true)
+
+                        batuCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[0])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            batuPlayer.setBackgroundResource(R.drawable.bg_click)
+                            batuCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                        kertasCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[1])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            batuPlayer.setBackgroundResource(R.drawable.bg_click)
+                            kertasCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                        guntingCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[2])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            batuPlayer.setBackgroundResource(R.drawable.bg_click)
+                            guntingCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                    }
+                }
+
+                kertasPlayer.setOnClickListener {
+                    setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, false)
+                    pilihanPlayer = pilihanSuit[1]
+                    player.setPilihanPlayer(pilihanPlayer)
+                    firstPlayer = false
+
+                    if (!firstPlayer) {
+                        setEnabledImageView(batuCom, kertasCom, guntingCom, true)
+
+                        batuCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[0])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            kertasPlayer.setBackgroundResource(R.drawable.bg_click)
+                            batuCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                        kertasCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[1])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            kertasPlayer.setBackgroundResource(R.drawable.bg_click)
+                            kertasCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                        guntingCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[2])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            kertasPlayer.setBackgroundResource(R.drawable.bg_click)
+                            guntingCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                    }
+
+                }
+
+                guntingPlayer.setOnClickListener {
+                    setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, false)
+                    pilihanPlayer = pilihanSuit[2]
+                    player.setPilihanPlayer(pilihanPlayer)
+                    firstPlayer = false
+
+                    if (!firstPlayer) {
+                        setEnabledImageView(batuCom, kertasCom, guntingCom, true)
+
+                        batuCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[0])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            guntingPlayer.setBackgroundResource(R.drawable.bg_click)
+                            batuCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                        kertasCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[1])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            guntingPlayer.setBackgroundResource(R.drawable.bg_click)
+                            kertasCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                        guntingCom.setOnClickListener {
+                            setEnabledImageView(batuCom, kertasCom, guntingCom, false)
+                            player2.setPilihanPlayer(pilihanSuit[2])
+                            duelSuit(true)
+                            hasilSuit = hasilSuit(
+                                player.getStatusMenang(),
+                                player2.getStatusMenang(),
+                                suitHasil
+                            )
+                            guntingPlayer.setBackgroundResource(R.drawable.bg_click)
+                            guntingCom.setBackgroundResource(R.drawable.bg_click)
+                            Log.d("pemenang", hasilSuit)
+                        }
+
+                    }
+
+                }
+
             }
 
 
             btnRefresh.setOnClickListener {
                 setEnabledImageView(batuPlayer, kertasPlayer, guntingPlayer, true)
                 setClearImageView(batuCom, guntingCom, kertasCom)
-                Toast.makeText(this@MainActivity,"Main lagi Kuy",Toast.LENGTH_SHORT).show()
+                firstPlayer = true
+                Toast.makeText(this@MainActivity, "Main lagi Kuy", Toast.LENGTH_SHORT).show()
             }
 
 
@@ -90,27 +260,42 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun duelSuit() {
-        player.suit(player.getPilihanPlayer(), computer.getPilihanPlayer())
-        computer.suit(computer.getPilihanPlayer(), player.getPilihanPlayer())
+    private fun duelSuit(gameMode1: Boolean) {
+        if (!gameMode1) {
+            player.suit(player.getPilihanPlayer(), computer.getPilihanPlayer())
+            computer.suit(computer.getPilihanPlayer(), player.getPilihanPlayer())
+        } else {
+            player.suit(player.getPilihanPlayer(), player2.getPilihanPlayer())
+            player2.suit(player.getPilihanPlayer(), player2.getPilihanPlayer())
+        }
     }
 
-    private fun pilihanComputer(imageView1: ImageView,imageView2: ImageView,imageView3: ImageView) {
+    private fun pilihanComputer(
+        imageView1: ImageView,
+        imageView2: ImageView,
+        imageView3: ImageView
+    ) {
         computer.pilihanCom(pilihanSuit)
         computer.setPilihanPlayer(computer.getPilihanCom())
         computer.efekPilihanCom(imageView1, imageView2, imageView3)
     }
 
-    private fun setClearImageView(imageView1: ImageView, imageView2: ImageView, imageView3: ImageView) {
+    private fun setClearImageView(
+        imageView1: ImageView,
+        imageView2: ImageView,
+        imageView3: ImageView
+    ) {
         imageView1.setBackgroundResource(R.drawable.bg_awal_click)
         imageView2.setBackgroundResource(R.drawable.bg_awal_click)
         imageView3.setBackgroundResource(R.drawable.bg_awal_click)
     }
 
 
-
-
-    private fun hasilSuit(status1: Boolean, status2: Boolean, arrayhasilSuit: Array<String>): String {
+    private fun hasilSuit(
+        status1: Boolean,
+        status2: Boolean,
+        arrayhasilSuit: Array<String>
+    ): String {
 
         val nilaiString: String = if (status1 && !status2) {
             arrayhasilSuit[0]
@@ -122,19 +307,20 @@ class MainActivity : AppCompatActivity() {
         return nilaiString
     }
 
-    private fun setEnabledImageView(imageView1: ImageView, imageView2: ImageView, imageView3: ImageView, setNilai: Boolean) {
+    private fun setEnabledImageView(
+        imageView1: ImageView,
+        imageView2: ImageView,
+        imageView3: ImageView,
+        setNilai: Boolean
+    ) {
         imageView1.isEnabled = setNilai
         imageView2.isEnabled = setNilai
         imageView3.isEnabled = setNilai
 
         if (setNilai) {
-            imageView1.isEnabled = setNilai
             imageView1.setBackgroundResource(R.drawable.bg_awal_click)
-            imageView2.isEnabled = setNilai
             imageView2.setBackgroundResource(R.drawable.bg_awal_click)
-            imageView3.isEnabled = setNilai
             imageView3.setBackgroundResource(R.drawable.bg_awal_click)
-
         }
 
     }
