3afa4f18c9aeccc519488c7a513d15bcfe99f82e
Gizogazarm <theblues11.helmi@gmail.com>
2023-04-20 09:35:33 +0700

    menghapus import log di mainActivity

--- app/src/main/java/com/example/gamesuit/MainActivity.kt
+++ app/src/main/java/com/example/gamesuit/MainActivity.kt
@@ -4,7 +4,6 @@ package com.example.gamesuit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import com.bumptech.glide.Glide
